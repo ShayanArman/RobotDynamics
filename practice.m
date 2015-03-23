c853637c18d7415cdd54e9e875fe4054943effda
Shayan Arman <shayan.arman2@gmail.com>
2015-03-22 18:30:38 -0700

    Created robot model and plotted initial position.

--- practice.m
+++ practice.m
@@ -1,11 +1,29 @@
 % q d a alpha
-% Length constants
-LENGTH_ONE = 0.352;
-LENGTH_TWO = 0.070;
-LENGTH_THREE = 0.360;
-LENGTH_FOUR = 0.445;
+% Link lengths
+LINK_LENGTH_ONE = 0.352;
+RADIUS_ONE = 0.070;
+LINK_LENGTH_TWO = 0.360;
+LINK_LENGTH_THREE = 0.445;
 
+% Join masses
 MASS_ONE = 50;
 MASS_TWO = 30;
 MASS_THREE = 35;
 
+% First joint
+j(1) = Link([0 LINK_LENGTH_ONE RADIUS_ONE pi/2]);
+
+% Second joint
+j(2) = Link([0 0 LINK_LENGTH_TWO 0]);
+
+% Third joint
+j(3) = Link([0 0 LINK_LENGTH_THREE 0]);
+
+% Connecting three links into a robot
+robot = SerialLink(j, 'name', 'Bender');
+
+% Initial position
+q_initial = [0 0 0];
+
+% Plot the initial position
+robot.plot(q_initial);
